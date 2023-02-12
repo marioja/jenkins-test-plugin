4244ea2595b5519c8833b1b9528982a9f5c2b3e8
Mario Jauvin <marioja@users.noreply.github.com>
2023-02-12 17:56:12 -0500

    create a simple jenkinsfile that can be built in hpi:run

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,11 @@
-/*
- See the documentation for more options:
- https://github.com/jenkins-infra/pipeline-library/
-*/
-buildPlugin(
-  useContainerAgent: true,
-  configurations: [
-    [platform: 'linux', jdk: 17], // use 'docker' if you have containerized tests
-    [platform: 'windows', jdk: 11],
-])
+pipeline {
+    stages {
+        stage('Build') {
+            steps {
+                withMaven() {
+                	bat "mvn verify"
+                }
+            }
+        }
+    }
+}
--- src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
+++ src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
@@ -1,10 +1,13 @@
 package io.jenkins.plugins.sample;
 
-import hudson.model.Action;
+import hudson.model.Run;
+import jenkins.model.RunAction2;
 
-public class HelloWorldAction implements Action {
+public class HelloWorldAction implements RunAction2 {
 	
 	private String name;
+	
+	private transient Run run;
 	
 	public HelloWorldAction(String aName) {
 		this.name = aName;
@@ -17,19 +20,35 @@ public class HelloWorldAction implements Action {
 	@Override
 	public String getIconFileName() {
 		// TODO Auto-generated method stub
-		return null;
+		return "document.png";
 	}
 
 	@Override
 	public String getDisplayName() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Greetings";
 	}
 
 	@Override
 	public String getUrlName() {
 		// TODO Auto-generated method stub
-		return null;
+		return "greeting";
+	}
+
+	@Override
+	public void onAttached(Run<?, ?> r) {
+		this.run = r;
+		
+	}
+
+	@Override
+	public void onLoad(Run<?, ?> r) {
+		this.run = r;
+		
+	}
+	
+	public Run getRun() {
+		return this.run;
 	}
 
 }
